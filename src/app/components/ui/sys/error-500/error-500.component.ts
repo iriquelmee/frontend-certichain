ab8bdb4af02d65ffca5a9c06c5dd6e4c6e93a96c
iriquelmee <i.riquelmee@duocuc.cl>
2025-05-26 01:53:14 -0400

    Add vista error 500

---
+++ src/app/components/ui/sys/error-500/error-500.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+
+@Component({
+  selector: 'app-error-500',
+  imports: [CardModule],
+  templateUrl: './error-500.component.html',
+  styleUrl: './error-500.component.scss'
+})
+export class Error500Component {
+
+}
