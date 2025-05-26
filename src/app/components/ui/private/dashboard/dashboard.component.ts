b16d47a650ed755481405818a41741414b7cb8e5
iriquelmee <i.riquelmee@duocuc.cl>
2025-05-26 01:48:54 -0400

    Add vista dashboard

---
+++ src/app/components/ui/private/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [CardModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+  title:string = "Dashboard";
+}
