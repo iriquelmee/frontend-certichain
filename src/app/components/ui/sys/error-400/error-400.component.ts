ec0fe678fd7563d15d7b8301d7bc09aecbab1e2a
iriquelmee <i.riquelmee@duocuc.cl>
2025-05-26 01:52:48 -0400

    Add vista error 400

---
+++ src/app/components/ui/sys/error-400/error-400.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+
+@Component({
+  selector: 'app-error-400',
+  imports: [CardModule],
+  templateUrl: './error-400.component.html',
+  styleUrl: './error-400.component.scss'
+})
+export class Error400Component {
+
+}
