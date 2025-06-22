c380caf5c544fcf25f6c3ea77e3efcba7d6d9780
iriquelmee <i.riquelmee@duocuc.cl>
2025-06-22 00:39:17 -0400

    Fix add ruta admin , se comentan rutas para vistas en blanco

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,7 @@ import { DemoComponent } from './components/ui/shared/demo/demo.component';
 import { Error400Component } from './components/ui/sys/error-400/error-400.component';
 import { Error500Component } from './components/ui/sys/error-500/error-500.component';
 import { AuthGuard } from './guards/auth.guard';
+import { AdminComponent } from './components/ui/private/admin/admin.component';
 
 export const routes: Routes = [
     {
@@ -31,14 +32,8 @@ export const routes: Routes = [
       component: ConfirmarRegistroComponent
     },
     {
-      path: 'dashboard',
-      component: DashboardComponent,
-      canActivate: [AuthGuard]
-    },
-    {
-      path: 'certificados',
-      component: CertificadosComponent,
-      canActivate: [AuthGuard]
+      path: 'admin',
+      component: AdminComponent      
     },
     {
       path: 'instituciones',
@@ -51,16 +46,6 @@ export const routes: Routes = [
       canActivate: [AuthGuard]
     },
     {
-      path: 'perfil',
-      component: ProfileComponent,
-      canActivate: [AuthGuard]
-    },    
-    {
-      path: 'demo',
-      component: DemoComponent,
-      canActivate: [AuthGuard]
-    },
-    {
       path: 'error-400',
       component: Error400Component
     },
@@ -68,6 +53,26 @@ export const routes: Routes = [
       path: 'error-500',
       component: Error500Component
     },
+    // {
+    //   path: 'dashboard',
+    //   component: DashboardComponent,
+    //   canActivate: [AuthGuard]
+    // },
+    // {
+    //   path: 'perfil',
+    //   component: ProfileComponent,
+    //   canActivate: [AuthGuard]
+    // },    
+    // {
+    //   path: 'demo',
+    //   component: DemoComponent,
+    //   canActivate: [AuthGuard]
+    // },
+    // {
+    //   path: 'certificados',
+    //   component: CertificadosComponent,
+    //   canActivate: [AuthGuard]
+    // },
     {
       path: '**',
       redirectTo: 'login'
