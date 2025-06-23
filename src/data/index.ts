2453135e61c05f573d8bd1f0f1d043ca1e9d075d
iriquelmee <i.riquelmee@duocuc.cl>
2025-06-23 00:27:57 -0400

    Add data tabs y sub vistas

--- src/data/index.ts
+++ src/data/index.ts
@@ -22,4 +22,21 @@ export const sidebarItems = [
     {
       separator: true
     }
-  ];  
+  ];
+  
+export const tabsAdmin = [
+  { title: 'Usuarios', value: 0, content: '', type: 'component', component: 'Usuarios'  },
+  { title: 'Categorias', value: 1, content: '', type: 'component', component: 'Categoria' },
+  { title: 'Auditoría', value: 2, content: '', type: 'component', component: 'Auditoria' },
+];
+
+export const tabsInstituciones = [
+  { title: 'Administrar Solicitudes', value: 0, content: '', type: 'component', component: 'SolicitudesInstitucion'  },
+  { title: 'Administrar Tipos Documentos', value: 1, content: '', type: 'component', component: 'TiposDocumentos' },
+  { title: 'Administrar Otras Solicitudes', value: 2, content: '', type: 'component', component: 'OtrasSolicitudes' },
+];
+
+export const tabsUsuarios = [
+  { title: 'Informacion Usuario', value: 0, content: '', type: 'component', component: 'InformacionUsuario'  },
+  { title: 'Solicitudes', value: 1, content: '', type: 'component', component: 'SolicitudesUsuario' },
+];
