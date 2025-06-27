bc18c33ba15be49d34a172f7ce0bbbadae5ca2d7
iriquelmee <i.riquelmee@duocuc.cl>
2025-06-27 14:43:55 -0400

    Add componente select reutilizable

---
+++ src/app/components/shared/select/select.component.ts
@@ -0,0 +1,54 @@
+import { Component, Input, Output, EventEmitter, forwardRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+
+@Component({
+  selector: 'app-select',
+  standalone: true,
+  imports: [CommonModule, DropdownModule, FormsModule],
+  templateUrl: './select.component.html',
+  styleUrl: './select.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectComponent),
+      multi: true
+    }
+  ]
+})
+export class SelectComponent implements ControlValueAccessor {
+  @Input() options: any[] = [];
+  @Input() optionLabel: string = 'label';
+  @Input() optionValue: string = 'value';
+  @Input() placeholder: string = 'Seleccionar';
+  @Input() disabled: boolean = false;
+  @Input() styleClass: string = '';
+  
+  value: any;
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  onSelectionChange(event: any) {
+    this.value = event.value;
+    this.onChange(this.value);
+    this.onTouched();
+  }
+}
