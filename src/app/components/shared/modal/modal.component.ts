982d256f715372914c8e7ad505919a0c9687a347
iriquelmee <i.riquelmee@duocuc.cl>
2025-06-27 14:55:47 -0400

    Fix se acomoda modal para recibir formularios de manera dinamica, para ser reutilizado en otras vistas

--- src/app/components/shared/modal/modal.component.ts
+++ src/app/components/shared/modal/modal.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
 import { ModalService } from '../../../services/shared/modal.service';
-
 
 @Component({
   selector: 'app-modal',
-  imports:[DialogModule],
+  standalone: true,
+  imports: [DialogModule, ButtonModule, CommonModule, ReactiveFormsModule],
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
@@ -14,7 +16,10 @@ export class ModalComponent implements OnInit {
   visible = false;
   title = '';
   content = '';
+  formData?: FormGroup;
+  isFormMode = false;
   onCloseCallback?: () => void;
+  onSaveCallback?: (data: any) => void;
 
   constructor(private modalService: ModalService) {}
 
@@ -26,6 +31,16 @@ export class ModalComponent implements OnInit {
     this.title = title;
     this.content = content;
     this.visible = true;
+    this.isFormMode = false;
+    this.onCloseCallback = onClose;
+  }
+
+  openWithForm(title: string, form: FormGroup, onSave?: (data: any) => void, onClose?: () => void) {
+    this.title = title;
+    this.formData = form;
+    this.visible = true;
+    this.isFormMode = true;
+    this.onSaveCallback = onSave;
     this.onCloseCallback = onClose;
   }
 
@@ -33,6 +48,13 @@ export class ModalComponent implements OnInit {
     this.visible = false;
     if (this.onCloseCallback) {
       this.onCloseCallback();
+    }
+  }
+
+  save() {
+    if (this.formData && this.formData.valid && this.onSaveCallback) {
+      this.onSaveCallback(this.formData.value);
+      this.visible = false;
     }
   }
 }
--- src/app/services/shared/modal.service.ts
+++ src/app/services/shared/modal.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { FormGroup } from "@angular/forms";
 import { ModalComponent } from "../../components/shared/modal/modal.component";
 
 @Injectable({ providedIn: 'root' })
@@ -9,8 +10,12 @@ export class ModalService {
     this.modalComponent = modal;
   }
 
-  show(title: string, content: string) {
-    this.modalComponent?.open(title, content);
+  show(title: string, content: string, onClose?: () => void) {
+    this.modalComponent?.open(title, content, onClose);
+  }
+
+  showForm(title: string, form: FormGroup, onSave?: (data: any) => void, onClose?: () => void) {
+    this.modalComponent?.openWithForm(title, form, onSave, onClose);
   }
 
   close() {
