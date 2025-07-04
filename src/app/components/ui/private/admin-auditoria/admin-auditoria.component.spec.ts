1fa131fc433109999bed7bb7993477c1169698ff
iriquelmee <i.riquelmee@duocuc.cl>
2025-07-04 17:21:52 -0400

    add cobertura pruebas unitarias admin-auditoria.component

--- src/app/components/ui/private/admin-auditoria/admin-auditoria.component.spec.ts
+++ src/app/components/ui/private/admin-auditoria/admin-auditoria.component.spec.ts
@@ -1,14 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
 import { AdminAuditoriaComponent } from './admin-auditoria.component';
+import { DocumentAuditService } from '../../../../services/audit/document-audit.service';
+import { PublicDocumentAuditLog } from '../../../../models/public-document-audit-log';
+import { TestingModule } from '../../../../../testing/test-module';
+import { CardModule } from 'primeng/card';
+import { of } from 'rxjs';
 
 describe('AdminAuditoriaComponent', () => {
   let component: AdminAuditoriaComponent;
   let fixture: ComponentFixture<AdminAuditoriaComponent>;
+  let documentAuditServiceMock: jasmine.SpyObj<DocumentAuditService>;
 
   beforeEach(async () => {
+    // mockeanding auditoria
+    const mockAuditLogs: PublicDocumentAuditLog[] = [
+      {
+        txID: '1',
+        operation: 'VIEW',
+        documentId: '123',
+        userId: 'ghost',
+        institution: 'Duoc Test',
+        timestamp: new Date().toISOString()
+      }
+    ];
+    
+    // mockeando docs
+    documentAuditServiceMock = jasmine.createSpyObj('DocumentAuditService', ['getPublicAuditLogs']);
+    documentAuditServiceMock.getPublicAuditLogs.and.returnValue(of(mockAuditLogs));
+    
+    // configurando modulo de pruebas
     await TestBed.configureTestingModule({
-      imports: [AdminAuditoriaComponent]
+      imports: [
+        AdminAuditoriaComponent, 
+        TestingModule,
+        FormsModule,
+        CardModule
+      ],
+      providers: [
+        { provide: DocumentAuditService, useValue: documentAuditServiceMock }
+      ]
     })
     .compileComponents();
 
