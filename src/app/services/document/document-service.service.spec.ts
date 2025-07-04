4bfa22a5a5f30821f2426f1d9aa0ee37d00a17d7
iriquelmee <i.riquelmee@duocuc.cl>
2025-07-04 17:25:11 -0400

    add cobertura pruebas unitarias document-type.service

--- src/app/services/document/document-service.service.spec.ts
+++ src/app/services/document/document-service.service.spec.ts
@@ -1,13 +1,24 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpTestingController } from '@angular/common/http/testing';
 import { DocumentServiceService } from './document-service.service';
+import { TestingModule } from '../../../testing/test-module';
 
 describe('DocumentServiceService', () => {
   let service: DocumentServiceService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [TestingModule],
+      providers: [DocumentServiceService]
+    });
+    
     service = TestBed.inject(DocumentServiceService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
